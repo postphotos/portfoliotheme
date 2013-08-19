7d1ea53ea9139be8654f6957b5593a49fbbe7e6d
Leo Postovoit <musicman5821@gmail.com>
2013-08-18 19:31:42 -0600

    footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+	</div><!-- Row End -->
+</section><!-- Container End -->
+
+<div class="row full-width">
+	<?php dynamic_sidebar("Footer"); ?>
+</div>
+
+<footer class="row full-width" role="contentinfo">
+	<div class="small-12 large-4 columns">
+		<p>&copy; <?php echo date('Y'); ?>, Leo Postovoit.</p>
+	</div>
+	
+	<div class="small-12 large-10 large-centered columns">
+		<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+
+<script>
+	(function($) {
+		$(document).foundation();
+	})(jQuery);
+</script>
+	
+</body>
+</html>
