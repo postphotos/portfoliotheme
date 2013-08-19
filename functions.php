bc5ecb7ad60305b9d102217c5f8cbf840e6536a8
Leo Postovoit <musicman5821@gmail.com>
2013-08-19 13:06:41 -0600

    added rel links

--- functions.php
+++ functions.php
@@ -13,25 +13,25 @@ anything else you may wan to use plugins to keep things tidy.
     - head cleanup
 	- post and images related cleaning
 */
-require_once('lib/clean.php'); // do all the cleaning and enqueue here
+require_once('../reverie/lib/clean.php'); // do all the cleaning and enqueue here
 /*
 2. lib/enqueue-sass.php or enqueue-css.php
     - enqueueing scripts & styles for Sass OR CSS
     - please use either Sass OR CSS, having two enabled will ruin your weekend
 */
 //require_once('lib/enqueue-sass.php'); // do all the cleaning and enqueue if you Sass to customize Reverie
-require_once('lib/enqueue-css.php'); // to use CSS for customization, uncomment this line and comment the above Sass line
+require_once('../reverie/lib/enqueue-css.php'); // to use CSS for customization, uncomment this line and comment the above Sass line
 /*
 3. lib/foundation.php
 	- add pagination
 	- custom walker for top-bar and related
 */
-require_once('lib/foundation.php'); // load Foundation specific functions like top-bar
+require_once('../reverie/lib/foundation.php'); // load Foundation specific functions like top-bar
 /*
 4. lib/presstrends.php
     - add PressTrends, tracks how many people are using Reverie
 */
-require_once('lib/presstrends.php'); // load PressTrends to track the usage of Reverie across the web, comment this line if you don't want to be tracked
+require_once('../reverie/lib/presstrends.php'); // load PressTrends to track the usage of Reverie across the web, comment this line if you don't want to be tracked
 
 /**********************
 Add theme supports
