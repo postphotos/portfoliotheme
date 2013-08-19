cf2aeaa1d69b81668de3616f622a5b607deecc10
Leo Postovoit <musicman5821@gmail.com>
2013-08-18 21:22:00 -0600

    adding hr to footer

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 	<?php dynamic_sidebar("Footer"); ?>
 </div>
 
-<footer class="row full-width" role="contentinfo">
+<footer class="row full-width" role="contentinfo"><hr>
 	<div class="small-12 large-4 columns">
 		<p>&copy; <?php echo date('Y'); ?>, Leo Postovoit.</p>
 	</div>
