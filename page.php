3942f7fa4a05fd2fa2e2fe7cfb9c682c3dfca8f7
Leo Postovoit <musicman5821@gmail.com>
2013-08-18 20:00:39 -0600

    adding page

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
 <!-- Row for main content area -->
-	<div class="small-12 large-10 large-centered columns" role="main">
+	<div class="small-12 columns" role="main">
 	
 	<?php /* Start loop */ ?>
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php reverie_entry_meta(); ?>
+				<!-- <?php reverie_entry_meta(); ?> -->
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
