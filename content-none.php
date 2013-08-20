08c0d8b6c15a3c680b29ec7f1dad4e5583e32271
Leo Postovoit <musicman5821@gmail.com>
2013-08-20 11:16:49 -0600

    adding easter egg

---
+++ content-none.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying a "No posts found" message.
+ *
+ * @subpackage Reverie
+ * @since Reverie 4.0
+ */
+?>
+
+<article id="post-0" class="post no-results not-found">
+	<header>
+		<h2><?php _e( 'Nothing Found', 'reverie' ); ?></h2>
+	</header>
+	<div class="entry-content small-centered">
+		<p><?php _e( 'Apologies, but no results were found.Perhaps searching will help find a related post.<br><br>If not, please look my beautiful dog.<br><img src="http://postphotos.us/p/wp-content/uploads/2013/08/kitty-easter.jpg">', 'reverie' ); ?></p>
+	</div>
+	<hr />
+</article>
