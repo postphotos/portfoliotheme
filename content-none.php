860de2b9907732803d12edbb2ceeeffe927e54a4
Leo Postovoit <musicman5821@gmail.com>
2013-08-20 11:20:34 -0600

    easter time

--- content-none.php
+++ content-none.php
@@ -12,7 +12,7 @@
 		<h2><?php _e( 'Nothing Found', 'reverie' ); ?></h2>
 	</header>
 	<div class="entry-content small-centered">
-		<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.<br><br>Or maybe you were just looking for a photo of my beautiful dog!<br><img src="http://postphotos.us/p/wp-content/uploads/2013/08/kitty-easter.jpg">', 'reverie' ); ?></p>
+		<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.<br><br>Or maybe you were just looking for a photo of my beautiful dog!<br><a href="http://postphotos.us/p/unicorns"><img src="http://postphotos.us/p/wp-content/uploads/2013/08/kitty-easter.jpg" class="small-centered"></a>', 'reverie' ); ?></p>
 	</div>
 	<hr />
 </article>
